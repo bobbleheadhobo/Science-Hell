b4168d4dbc805a58161059731b9f2cb25773216c
Chey Salem <csalem@nmsu.edu>
2024-04-10 00:41:24 -0600

    added font colors

--- chey/globals/Types.gd
+++ chey/globals/Types.gd
@@ -6,3 +6,11 @@ enum ItemTypes{
 	KEY,
 	QUEST_ITEM,
 }
+
+
+# Colors for font
+const COLOR_NPC = Color("#cc0000")
+const COLOR_ITEM = Color("#3300cc")
+const COLOR_SPEECH = Color("#00b32d")
+const COLOR_LOCATION = Color("#ffbf00")
+const COLOR_SYSTEM = Color("#6b6161")
