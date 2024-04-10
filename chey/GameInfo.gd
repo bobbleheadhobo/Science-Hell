b4e3de8fec32f89c27ce7086834717a01634675d
Chey Salem <csalem@nmsu.edu>
2024-04-10 00:00:52 -0600

    refactored

---
+++ chey/GameInfo.gd
@@ -0,0 +1,74 @@
+extends PanelContainer
+
+# Response scene
+const Response = preload("res://chey/input/Response.tscn")
+# script class not instance of game => save data
+const InputResponse = preload("res://chey/input/InputResponse.tscn")
+
+# value can be edited in editor with export
+@export var max_lines_remembered := 30
+#var max_scroll_length := 0
+
+@onready var scroll = $Scroll
+# connect to the property
+@onready var scrollbar = scroll.get_v_scroll_bar()
+
+@onready var history_rows = $Scroll/HistoryRows
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# connect scroll bar changed signal to Game.gd
+	scrollbar.connect("changed",_handle_scrollbar_changed)
+	#max_scroll_length = scrollbar.max_value # new
+	
+	
+
+##### PUBLIC #####
+
+# generate response for each room for player. responds to anything
+func create_response(response_text: String):
+	# instance of Respone and add response
+	var response = Response.instantiate()
+	# eventually use export var or JSON
+	response.text = response_text
+	_add_response_to_game(response)
+	
+# generate response for each room for player. responds to anything
+func create_response_with_input(response_text: String, input_text: String):
+	var input_response = InputResponse.instantiate()
+	
+	# show user input (validation and scroll scroll history), new response from command processor
+	input_response.set_text(input_text, response_text)
+	# add response to game
+	_add_response_to_game(input_response)
+
+##### PRIVATE #####
+# PRE connected scroll bar signal coming from ready
+# POST handles scrollbar vertical action
+func _handle_scrollbar_changed():
+	# implement scroll to bottom
+	#if max_scroll_length != scrollbar.max_value:
+		#max_scroll_length = scrollbar.max_value # new
+	scroll.scroll_vertical = scrollbar.max_value
+
+
+# PRE: called when on input sibmitted function is active
+# POST: deletes msgs < 30 msgs
+func _delete_history_beyond_limit():
+	if history_rows.get_child_count() > max_lines_remembered:
+		# get the history rows children
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		# go through history rows 
+		for i in range(rows_to_forget):
+			# delete child node from tree and memory
+			history_rows.get_child(i).queue_free()
+			
+			
+			
+# PRE: PRIVATE FUNCTION from Response scene branch
+# POST: adds response to game
+func _add_response_to_game(response: Control):
+	# add scene to child as history rows
+	history_rows.add_child(response)
+	# deletes msg < 30 msgs
+	_delete_history_beyond_limit()
