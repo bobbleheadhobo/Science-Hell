e0255f34f6f6661ea9ca263b1ff700fa65bdf038
Chey Salem <csalem@nmsu.edu>
2024-04-10 00:49:42 -0600

    helper color font functions

--- chey/globals/Types.gd
+++ chey/globals/Types.gd
@@ -14,3 +14,25 @@ const COLOR_ITEM = Color("#3300cc")
 const COLOR_SPEECH = Color("#00b32d")
 const COLOR_LOCATION = Color("#ffbf00")
 const COLOR_SYSTEM = Color("#6b6161")
+
+
+
+# helper functions
+func wrap_npc_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_NPC.to_html(), text]
+
+
+func wrap_item_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_ITEM.to_html(), text]
+
+
+func wrap_speech_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_SPEECH.to_html(), text]
+
+
+func wrap_location_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_LOCATION.to_html(), text]
+
+
+func wrap_system_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_SYSTEM.to_html(), text]
