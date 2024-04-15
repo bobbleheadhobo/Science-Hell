df418f9b2bbd2cfb9d90c98bf77701a1f8da3abf
Jweston1080 <jasonweston1080@gmail.com>
2024-04-14 23:23:42 -0600

    adjusted note sizes
    made note sizes match

--- level1(jason)/Scripts/Game.gd
+++ level1(jason)/Scripts/Game.gd
@@ -105,7 +105,7 @@ func _on_Conductor_beat(current_position):
 		spawn_2_beat = 0
 		spawn_3_beat = 0
 		spawn_4_beat = 0
-	if song_position_in_beats > 404:
+	if song_position_in_beats > 200:
 		Global.set_score(score)
 		Global.combo = max_combo
 		Global.great = great
