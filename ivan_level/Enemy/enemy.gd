24b694bc3cf19851befda0935b876c4ece026f3c
Pat <107898813+pjojola@users.noreply.github.com>
2024-04-17 13:18:25 -0600

    Fix to physics of enemies

--- ivan_level/Enemy/enemy.gd
+++ ivan_level/Enemy/enemy.gd
@@ -4,10 +4,14 @@ var speed = 25
 var player_chase = false
 var player = null
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position) / speed
-		
+		# Add the gravity.
+		velocity.y += gravity * delta
+	
 		
 
 func _on_detection_area_body_entered(body):
