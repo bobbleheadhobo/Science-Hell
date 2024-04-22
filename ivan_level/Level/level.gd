eb5bec0c811047d6ac472e5d48b5132b7724b624
Pat <107898813+pjojola@users.noreply.github.com>
2024-04-22 17:22:45 -0600

    Addition of Global Pause

--- ivan_level/Level/level.gd
+++ ivan_level/Level/level.gd
@@ -1,4 +1,22 @@
 extends Node2D
 
+@onready var pause_menu = $Player/Pause
+var paused = false
+
 func _ready():
 	MusicManager.play_song("ivan")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
