027c9493b7967a676f5dc2b8e5c95a91bdb69b8f
Chey Salem <csalem@nmsu.edu>
2024-04-24 02:29:21 -0600

    colors

--- chey/globals/Types.gd
+++ chey/globals/Types.gd
@@ -11,11 +11,12 @@ enum ItemTypes{
 # Colors for font
 const COLOR_NPC = Color("#000080")
 const COLOR_ITEM = Color("#800080")
-const COLOR_SPEECH = Color("#00b32d")
+const COLOR_SPEECH = Color("#008080")
 const COLOR_LOCATION = Color("#228B22")
 const COLOR_SYSTEM = Color("#8B0000")
 
-
+const COLOR_MAIN = Color("#008080")
+const COLOR_DESCRIPTION = Color("#333333")
 
 # helper functions
 func wrap_npc_text(text: String) -> String:
@@ -36,3 +37,11 @@ func wrap_location_text(text: String) -> String:
 
 func wrap_system_text(text: String) -> String:
 	return "[color=#%s]%s[/color]" % [COLOR_SYSTEM.to_html(), text]
+
+
+func wrap_main_text(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_MAIN.to_html(), text]
+
+
+func wrap_main_description(text: String) -> String:
+	return "[color=#%s]%s[/color]" % [COLOR_DESCRIPTION.to_html(), text]
