237f931f61c02a013c08402b8ef9a5c3a2f5690d
Joey Troyer <supersoup4@gmail.com>
2024-04-24 18:05:59 -0600

    made pause screen global

---
+++ science_hell/scripts/ui_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var pause_scene = preload("res://science_hell/gui/Pause/pause.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		var canvas_layer = CanvasLayer.new()
+		add_child(canvas_layer)
+		var pause = pause_scene.instantiate()
+		canvas_layer.add_child(pause)
