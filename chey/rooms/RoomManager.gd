8adf7f66e9122e51d148f15d93435d5930dd316e
Chey Salem <csalem@nmsu.edu>
2024-04-27 21:04:49 -0600

    fixed room layouts and NPCs

--- chey/rooms/RoomManager.gd
+++ chey/rooms/RoomManager.gd
@@ -38,13 +38,7 @@ func _ready() -> void:
 	
 	$TextRoom.connect_exit_unlocked("west", $HintRoom)
 	
-	# cooper 2 room
-	exit = $TerminalRoom.connect_exit_locked("east", $Cooper2Room)
-	$Cooper2Room.connect_exit_locked("east", $ExitRoom)
-	#var duckieToken = load_item("DuckieToken")
-	var cooper2 = load_npc("Cooper2")
-	#$Cooper2Room.add_item(duckieToken)
-	$Cooper2Room.add_npc(cooper2)
+
 	
 	# hint room
 	$HintRoom.connect_exit_unlocked("east", $TextRoom)
@@ -60,26 +54,23 @@ func _ready() -> void:
 	# bash room
 	$BashRoom.connect_exit_unlocked("east", $CodeShRoom)
 	var bash_duckie = load_npc("BashDuckie")
-	$BashRoom.add_npc(bash_duckie)
-	
-	
-	
-	
-	# code.sh room
-	$CodeShRoom.connect_exit_unlocked("east", $CooperRoom)
 	var bats = load_npc("Bats")
-	$CodeShRoom.add_npc(bats)
-
-
+	$BashRoom.add_npc(bash_duckie)
+	$BashRoom.add_npc(bats)
 	
+	#### EAST OF TERMINAL ####
+	# cooper 2 room
+	$TerminalRoom.connect_exit_unlocked("east", $Cooper2Room)
+	$Cooper2Room.connect_exit_unlocked("east", $ExitRoom)
+	var sh_duckie = load_npc("SHDuckie")
+	var cooper2 = load_npc("Cooper2")
+	$Cooper2Room.add_npc(cooper2)
+	$Cooper2Room.add_npc(sh_duckie)
+
 	# SH terminal Exit room
 	$ExitRoom.connect_exit_unlocked("west", $Cooper2Room)
-	var exitToken = load_item("DuckieToken")
 	var sh_cooper = load_npc("SHCooper")
-	var sh_duckie = load_npc("SHDuckie")
-	$ExitRoom.add_item(exitToken)
 	$ExitRoom.add_npc(sh_cooper)
-	$ExitRoom.add_npc(sh_duckie)
 	
 	#sh_duckie.quest_reward = SceneManager.change_scene('sciencehall')
 	# if quest item recieved to cooper
