80360acaa267a3cafea000e7fc545840e5054cc1
Pat <107898813+pjojola@users.noreply.github.com>
2024-04-29 14:11:01 -0600

    Revert "Merge branch 'dev' into pat"
    This reverts commit 93ec626e39c8d895bed40f963b422624fb05972e, reversing
    changes made to 20c16d07bef7b1ae3ac42995969c0f1b56140f7a.

--- science_hell/scripts/PointLight2D.gd
+++ science_hell/scripts/PointLight2D.gd
@@ -9,23 +9,3 @@ func _on_lab_area_body_entered(body):
 func _on_lab_area_body_exited(body):
 	if(body.has_method("player")):
 		point.set_visible(true)
-
-
-func _on_aud_area_body_entered(body):
-	if(body.has_method("player")):
-		point.set_visible(false)
-
-
-func _on_aud_area_body_exited(body):
-	if(body.has_method("player")):
-		point.set_visible(true)
-
-
-func _on_bathroom_1_area_body_entered(body):
-	if(body.has_method("player")):
-		point.set_visible(false)
-
-
-func _on_bathroom_1_area_body_exited(body):
-	if(body.has_method("player")):
-		point.set_visible(true)
