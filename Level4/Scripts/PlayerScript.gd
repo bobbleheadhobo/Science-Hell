fbc1a8f23be04c022c65d6149c1ed1f7e5e368ed
Jonathan Barboza Morales <jonathan10barboza@gmail.com>
2024-05-09 22:37:22 -0600

    Added Most Recent Version of Parth's Level
    -finally managed to add my level progress to dev progress
    -changed input map to include my "sprint" action in my level
    -changed my player code so that 'space' is used instead of 'j' to jump
    -added collisions for bathroom

--- Level4/Scripts/PlayerScript.gd
+++ Level4/Scripts/PlayerScript.gd
@@ -14,13 +14,14 @@ signal healthChanged
 # @onready var hurttimer = something
 @onready var camera = $PlayerView
 @onready var sprite = $PlayerSprite
+@onready var ActiveHitbox = $PowerField
 
 var isHurt: bool = false
 var prevDir = 0
 var horizontalDirection = 1
 var wallJumpPushBack = 100
 var gravity = 10
-var jumpForce = 185
+var jumpForce = 200
 var stompForce = 400
 
 # function used to set up camera properly
@@ -28,8 +29,14 @@ func _ready():
 	camera.zoom.x = 3
 	camera.zoom.y = 3
 	
+	# begin with any active hitboxes disabled
+	ActiveHitbox.get_node("PowerBox").disabled = true
+	ActiveHitbox.get_node("ForceRush1").visible = false
+	ActiveHitbox.get_node("ForceRush2").visible = false
+	
 	# add Player to colliding list
 	add_to_group("Player")
+	
 
 func _physics_process(delta):
 	
@@ -47,7 +54,7 @@ func _physics_process(delta):
 			velocity.y = 999
 		
 	# handle jump input
-	if(Input.is_action_just_pressed("jump") && is_on_floor()):
+	if(Input.is_action_just_pressed("shoot") && is_on_floor()):
 		velocity.y = -jumpForce
 		
 	# handle wall jump input to the right
@@ -62,17 +69,27 @@ func _physics_process(delta):
 		
 	# if statement used to increase velocity if player
 	# holds the same direction for a while
-	if(prevDir == horizontalDirection && horizontalDirection != 0):
+	if(prevDir == horizontalDirection && horizontalDirection != 0 && Input.is_action_pressed("sprint")):
 		
+		# MADE PLAYER SPEED UP FASTER FOR DEMO PURPOSES
+		# FIXME, AND ALSO VIRUS RUSH SIGNAl
 		theAcc += 1
-		if(theAcc < 50):
+		if(theAcc < 25):
 			speed = 100
 		elif(theAcc < 150):
+			ActiveHitbox.get_node("ForceRush1").visible = true
+			ActiveHitbox.get_node("PowerBox").disabled = false
 			speed = 200
 		else:
+			ActiveHitbox.get_node("ForceRush1").visible = false
+			ActiveHitbox.get_node("ForceRush2").visible = true
+			ActiveHitbox.get_node("PowerBox").disabled = false
 			speed = 350
 		velocity.x = speed * horizontalDirection
 	else:
+		ActiveHitbox.get_node("ForceRush1").visible = false
+		ActiveHitbox.get_node("ForceRush2").visible = false
+		ActiveHitbox.get_node("PowerBox").disabled = true
 		theAcc = 1
 		speed = 100
 		velocity.x = speed * horizontalDirection
@@ -88,9 +105,9 @@ func _physics_process(delta):
 	set_velocity(velocity)
 	move_and_slide()
 	
-	print("Direction: ", horizontalDirection)
-	print("Acceleration: ", theAcc)
-	print("Speed: ", speed)
+	#print("Direction: ", horizontalDirection)
+	#print("Acceleration: ", theAcc)
+	#print("Speed: ", speed)
 
 func handleSprite(currDirection):
 	
@@ -99,13 +116,24 @@ func handleSprite(currDirection):
 	
 	if(currDirection == -1):
 		sprite.set_texture(textureLeft)
-	else:
+		ActiveHitbox.get_node("ForceRush1").position.x = -4
+		ActiveHitbox.get_node("ForceRush2").position.x = -4
+		ActiveHitbox.get_node("ForceRush1").flip_h = true
+		ActiveHitbox.get_node("ForceRush2").flip_h = true
+		
+	elif(currDirection == 1):
 		sprite.set_texture(textureRight)
+		ActiveHitbox.get_node("ForceRush1").position.x = 4
+		ActiveHitbox.get_node("ForceRush2").position.x = 4
+		ActiveHitbox.get_node("ForceRush1").flip_h = false
+		ActiveHitbox.get_node("ForceRush2").flip_h = false
+	else:
+		pass
 
 # tried to implement bouncing off of enemies
 func _on_signal_hurt_box_body_entered(body):
 	
-	if(body == $basicVirus && Input.is_action_just_pressed("jump")):
+	if(body == $basicVirus && Input.is_action_just_pressed("shoot")):
 		velocity.y = -jumpForce
 	elif(body == $basicVirus):
 		velocity.y = -stompForce
